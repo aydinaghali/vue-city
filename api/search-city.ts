7626fcddb015e5682f7c73745f2347aa767f5c8c
aydin <aydin.aghaliyev@gmail.com>
2024-08-20 23:17:25 +0400

    Add kysely

--- api/search-city.ts
+++ api/search-city.ts
@@ -1,18 +1,21 @@
-import { sql } from '@vercel/postgres';
+import { createKysely } from '@vercel/postgres-kysely';
+import { DB } from 'kysely-codegen';
 import { VercelRequest, VercelResponse } from '@vercel/node';
  
 export default async function handler(
   request: VercelRequest,
-  response: VercelResponse,
+  response: VercelResponse
 ) {
   try {
-    const cityName = request.query.cityName as string;
-    if (!cityName) throw new Error('Name required');
-    await sql`SELECT * FROM geoname WHERE name = '${cityName}';`;
+    //const db = createKysely<DB>();
+    let cityName = request.query.cityName as string;
+    if(!cityName) throw new Error('Name required');
+    //const result = await db.selectFrom("geoname").selectAll().where("name", "=", cityName).executeTakeFirst();
+    const result = "hello";
+    return response.status(500).json({ result });
   } catch (error) {
     return response.status(500).json({ error });
   }
- 
-  //const pets = await sql`SELECT * FROM Pets;`;
+  
   return response.status(200).json({});
 }
